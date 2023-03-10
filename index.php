5ab63983c1f85790f2fae140aaa853ddd7bb3423
SevensRequiem <externalthunder@gmail.com>
2023-03-10 09:50:47 -0600

    @ 0.2

--- index.php
+++ index.php
@@ -2,6 +2,8 @@
 <html lang="en" >
 <head>
 	<script src="https://cdn.jsdelivr.net/gh/foobar404/wave.js/dist/bundle.js"></script>
+	<script src="./assets/js/audio.js"></script>
+	<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
   <meta charset="UTF-8">
   <link rel="stylesheet" href="./style.css">
 
@@ -28,29 +30,27 @@
 								<li style="float:right;"><a><btn>youtube</btn></a></li>
 								<li style="float:right;"><a>[]</a></li>
 		</ul>
-		<div style="display: flex;"> 
-			<div id="musicbox">
-				<audio id="audiosrc" controls>
-					<source src="./assets/music/love like that.mp3" type="audio/mpeg">
-				  </audio>
-				<span class="current" id="num">0:00</span>
-				<span class="duration" id="num">0:00</span>
-				<a><div class="track">title</div></a>
-					  <div class="controls" style="font-size: 13px;">
-						<a title="song list"><btn>===</btn></a>
-						  <a title="play / pause"><btn>>/#</btn></a>
-							<a title="what do u think it does"><btn>repeat</btn></a>
-								<a title="previous"><btn><<</btn></a>
-								<a title="next"><btn>>></btn></a>
-								<canvas id="audcanvas" width="200" height="100" style="border:1px solid #000000;"></canvas>
-						</div>
+</nav>
+<div>version 1.4!!</div>
+<div style="display: flex;"> 
+			<audio preload autoplay></audio>
+<div id="playlist" style="display: none;"><ol>
+	<?php
+      //list mp3 files
+      $directory = './assets/music/';
+      $files = array_map('basename', glob($directory . "*.{mp3}", GLOB_BRACE));
+      foreach($files as $file) {
+        echo "<li><a href='#' data-src='./assets/music/$file'>$file</a></li>";
+      } {
+      }
+      ?></ol>
+</div>
+
 			</div>
-			<div id="statbox" style="float:right;">
-				<span>uptime<span style="font-size: 13px;" id="num">0:0:0:0</span></span>
-				<div>[] [] []</div>
-			</div> 
-		</nav>
 		<div id="content">
+		</div>
+		<div id="footer">
+			footer
 		</div>
 		</div>
 </body>
@@ -82,36 +82,35 @@ function load_server() {
 }
 </script>
 <script>
-	let audioElement = document.querySelector("#audiosrc");
-let canvasElement = document.querySelector("#audcanvas");
-let wave = new Wave(audioElement, canvasElement);
+      $(function() { 
+        // Setup the player to autoplay the next track
+        var a = audiojs.createAll({
+          trackEnded: function() {
+            var next = $('ol li.playing').next();
+            if (!next.length) next = $('ol li').first();
+            next.addClass('playing').siblings().removeClass('playing');
+            audio.load($('a', next).attr('data-src'));
+            audio.play();
+          }
+        });
+        
+        // Load in the first track
+        var audio = a[0];
+            first = $('ol a').attr('data-src');
+        $('ol li').first().addClass('playing');
+        audio.load(first);
 
-// Simple example: add an animation
-wave.addAnimation(new wave.animations.Wave());
-
-// Intermediate example: add an animation with options
-wave.addAnimation(new wave.animations.Wave({
-    lineWidth: 10,
-    lineColor: "red",
-    count: 20
-}));
-
-// Expert example: add multiple animations with options
-wave.addAnimation(new wave.animations.Square({
-    count: 50,
-    diamater: 300
-}));
-
-wave.addAnimation(new wave.animations.Glob({
-    fillColor: {gradient: ["red","blue","green"], rotate: 45},
-    lineWidth: 10,
-    lineColor: "#fff"
-}));
-
-// The animations will start playing when the provided audio element is played
-
-// 'wave.animations' is an object with all possible animations on it.
-
-// Each animation is a class, so you have to new-up each animation when passed to 'addAnimation'
-
+        // Load in a track on click
+        $('ol li').click(function(e) {
+          e.preventDefault();
+          $(this).addClass('playing').siblings().removeClass('playing');
+          audio.load($('a', this).attr('data-src'));
+          audio.play();
+        });
+      });
+    </script>
+<script>
+function ptoggle () {
+    $("#playlist").toggle();
+} 
 </script>
