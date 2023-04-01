443b3dff9c0496c66e590c2ac975957412095c59
requiem.moe <externalthunder@gmail.com>
2023-04-01 06:00:11 -0500

    Update edit.php

--- edit.php
+++ edit.php
@@ -1,9 +1,26 @@
-<?php
-require './includes/discord.php';
-function has_admin($guildid, $adminid)
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<title>Markdown Editor</title>
+	<link rel="stylesheet" type="text/css" href="https://cdn.jsdelivr.net/simplemde/latest/simplemde.min.css">
+	<script src="https://cdn.jsdelivr.net/simplemde/latest/simplemde.min.js"></script>
+</head>
+<body>
+	<?php
+    error_reporting(E_ERROR);
+    require "./includes/discord.php";
+    //has admin
+    function has_admin($guildid, $adminid)
 {
-    $url = $GLOBALS['base_url'] . "/api/guilds/$guildid/members/" . $_SESSION['user_id'];
-    $headers = array('Content-Type: application/json', 'Authorization: Bot ' . $GLOBALS['bot_token']);
+    $url =
+        $GLOBALS["base_url"] .
+        "/api/guilds/$guildid/members/" .
+        $_SESSION["user_id"];
+    $headers = [
+        "Content-Type: application/json",
+        "Authorization: Bot " . $GLOBALS["bot_token"],
+    ];
     $curl = curl_init();
     curl_setopt($curl, CURLOPT_URL, $url);
     curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
@@ -11,62 +28,40 @@ function has_admin($guildid, $adminid)
     $response = curl_exec($curl);
     curl_close($curl);
     $results = json_decode($response, true);
-    $roles = $results['roles'];
-    return in_array($adminid, $roles);
+    $roles = $results["roles"];
+    return is_array($roles) && in_array($adminid, $roles);
 }
-// Load Parsedown library for Markdown parsing
-require_once './vendor/autoload.php';
-$Parsedown = new Parsedown();
-
-if (!isset($_SESSION['user'])) {
-    header('Location: login.php');
-    exit;
-}
-
-// Get post UUID from query string parameter
-$postId = $_GET['post'];
-
-// Set post file path
-$post_file = './data/posts/' . $postId . '/post.md';
-
-// Check if post file exists
-if (!file_exists($post_file)) {
-    echo 'Post not found.';
-    exit;
-}
-
-// Load post content from file
-$post_content = file_get_contents($post_file);
-
-// Handle form submission
-if (($_SERVER['REQUEST_METHOD'] === 'POST') && (has_admin($guildid, $adminid))) {
-
-    // Get new post content from form data
-    $new_post_content = $_POST['content'];
-
-    // Save new post content to file
-    $result = file_put_contents($post_file, $new_post_content);
-
-    // Display success or error message
-    if ($result !== false) {
-        echo '<p>Post saved successfully.</p>';
-    } else {
-        echo '<p>Error saving post.</p>';
-    }
-}
-?>
-<!DOCTYPE html>
-<html>
-<head>
-    <title>Edit Post | Requiem</title>
-</head>
-<body>
-    <h1>Edit Post</h1>
-    <form method="post">
-        <textarea name="content" rows="20" cols="80"><?php echo $post_content; ?></textarea>
-        <br>
-        <input type="submit" value="Save">
-    </form>
-    <p><a href="blog.php">Back to Blog</a></p>
+	if (isset($_GET['post']) && has_admin($guildid, $adminid)) {
+        function save_post($post_id, $post_content) {
+            $post_dir = './data/posts/' . $post_id . '/';
+            $post_md_file = $post_dir . 'post.md';
+            $new_post_content = htmlspecialchars($post_content, ENT_QUOTES | ENT_SUBSTITUTE, 'UTF-8');
+            $result = file_put_contents($post_md_file, $new_post_content);
+            return ($result !== false);
+        }        
+		// Get post ID from URL
+		$postId = $_GET['post'];
+		// Get post data
+		$postJsonPath = './data/posts/' . $postId . '/post.json';
+		$postData = json_decode(file_get_contents($postJsonPath), true);
+		$postContent = file_get_contents('./data/posts/' . $postId . '/post.md');
+		// Display editor
+		echo '<form method="post">';
+		echo '<textarea id="editor" name="content">' . $postContent . '</textarea>';
+		echo '<input type="hidden" name="post_id" value="' . $postId . '">';
+		echo '<input type="submit" name="submit" value="Save">';
+		echo '</form>';
+	} else {
+		// Display error message if post ID is not set
+		echo '<p>Forbidden</p>';
+	}
+	?>
+	<script>
+		var editor = new SimpleMDE({
+			element: document.getElementById("editor"),
+			autofocus: true,
+			spellChecker: false
+		});
+	</script>
 </body>
 </html>
